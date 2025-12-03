3c7a58dcbf5b56575ff780523b8e78488bdbf58d
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-12-03 11:17:12 +0530

    day 1

---
+++ 2025/day-1/1.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::str::FromStr;
+
+fn main() {
+    p1();
+}
+
+fn p1() {
+    let input_string = fs::read_to_string("input.txt").unwrap();
+    let input: Vec<_> = input_string.trim().split("\n").collect();
+    let mut arrow = 50;
+    let mut z_c = 0;
+    for i in input.iter() {
+        let shift = &i[..1];
+        let num = <isize>::from_str(&i[1..]).unwrap();
+        let a;
+        match shift {
+            "L" => {
+                a = arrow - num;
+            }
+            "R" => {
+                a = arrow + num;
+            }
+            _ => todo!(),
+        }
+        arrow = a.rem_euclid(100);
+        if arrow == 0 {
+            z_c += 1;
+        }
+    }
+
+    println!("{z_c}");
+}
